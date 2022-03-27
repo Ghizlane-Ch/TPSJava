2b6aae8549bdfe4bc9f4011347936d89ad43297b
Ghizlane Ch <chtiteh.ghizlane.gmail.com>
2022-03-27 13:23:37 +0200

    sec c

--- src/main/java/cigma/pfe/MonApplication.java
+++ src/main/java/cigma/pfe/MonApplication.java
@@ -5,19 +5,13 @@ import cigma.pfe.Controllers.ClientController;
 import cigma.pfe.Models.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class MonApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
         ClientController ctrl = (ClientController)context.getBean("controller");
 
         Client clt = new Client("Zahira");
-
         ctrl.save(clt);
 
     }
 }
-
-
